b54a8e3c7f3e706c9bd0238b89e3a17878606614
Dasihub <dasihub02@gmail.com>
2024-02-27 01:22:00 +0600

    cors

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ const logger = require('morgan')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose')
-require('./models/userModel')
 
 const app = express()
 const PORT = process.env.PORT
@@ -13,7 +12,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(logger('dev'))
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({origin: 'http://localhost:3000', credentials: true}))
 
 app.use('/api', require('./routes'))
 
